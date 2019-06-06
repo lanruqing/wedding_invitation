364af5e6c732cd05c7ddcc80f0a5336b79d871b5
lanruqing <ruqing.lan@accenture.com>
2019-06-06 16:07:01 +0800

    ffff

--- es6/main.js
+++ es6/main.js
@@ -48,93 +48,95 @@ function pageInit(){
     })
   })
 }
-window.onload = () => {
-  if (document.body.clientWidth <= 580) {
-    document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 12 + 'px';
-    console.log('mobile');
-  } else if (document.body.clientWidth <= 1000) {
-    document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 15 + 'px';
-    console.log('tablet');
-  } else {
-    document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 20 + 'px';
-    console.log('pc');
-  }
-  var bgm = document.getElementById('bgm');
-        bgm.oncanplaythrough = () => {
-          setTimeout(function () {
-            document.getElementById('loading_container').style.display = 'none';
-            document.getElementById('need_music').classList.add('active');
-            var music_init = Array.from(document.querySelectorAll('.music_init'));
-            music_init.forEach((m, i) => {
-              let val = m.getAttribute('value');
-              console.log(val)
-              m.onclick = () => {
-                MusicStatus = JSON.parse(val)
-                music_init[0].classList.remove('choosed');
-                music_init[1].classList.remove('choosed')
-                m.classList.add('choosed')
-                if (MusicStatus) {
-                  bgm.play()
-                } else {
-                  bgm.pause()
-                }
-                setTimeout(function () {
-                  document.getElementById('loading').classList.add('slideOutUp')
-                }, 1000)
+
+if (document.body.clientWidth <= 580) {
+  document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 12 + 'px';
+  console.log('mobile');
+} else if (document.body.clientWidth <= 1000) {
+  document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 15 + 'px';
+  console.log('tablet');
+} else {
+  document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 20 + 'px';
+  console.log('pc');
+}
+var bgm = document.getElementById('bgm');
+      bgm.oncanplaythrough = () => {
+        setTimeout(function () {
+          document.getElementById('loading_container').style.display = 'none';
+          document.getElementById('need_music').classList.add('active');
+          var music_init = Array.from(document.querySelectorAll('.music_init'));
+          music_init.forEach((m, i) => {
+            let val = m.getAttribute('value');
+            console.log(val)
+            m.onclick = () => {
+              MusicStatus = JSON.parse(val)
+              music_init[0].classList.remove('choosed');
+              music_init[1].classList.remove('choosed')
+              m.classList.add('choosed')
+              if (MusicStatus) {
+                bgm.play()
+              } else {
+                bgm.pause()
+              }
+              setTimeout(function () {
+                document.getElementById('loading').classList.add('slideOutUp')
+              }, 1000)
+            }
+          })
+        }, 3000)
+      }
+var main_swiper = new Swiper('#main_swiper', {
+  direction: 'vertical',
+  noSwiping: true,
+  updateOnImagesReady : true,
+  lazy: {
+    loadPrevNext: true,
+    loadPrevNextAmount: 2,
+  },
+  on: {
+    init: function () {
+      swiperAnimateCache(this);
+      swiperAnimate(this);
+      var wi_swiper = new Swiper('#our_story', {
+        centeredSlides: true,
+        spaceBetween: 20,
+        slidesPerView: 1.1,
+        width: 350,
+        pagination: {
+          el: '.our_story_pagination',
+          clickable: true,
+          renderBullet: (index, className) => {
+            return '<span class="ourStory_pagination ' + className + '"></span>';
+          }
+        },
+        on: {
+          init() {
+            var storys = Array.from(document.querySelectorAll('.storys'));
+            storys.forEach((s, i) => {
+              s.onclick = () => {
+                wi_swiper.slideTo(i)
               }
             })
-          }, 3000)
-        }
-  var main_swiper = new Swiper('#main_swiper', {
-    direction: 'vertical',
-    noSwiping: true,
-    updateOnImagesReady : true,
-    lazy: {
-      loadPrevNext: true,
-      loadPrevNextAmount: 2,
-    },
-    on: {
-      init: function () {
-        swiperAnimateCache(this);
-        swiperAnimate(this);
-        var wi_swiper = new Swiper('#our_story', {
-          centeredSlides: true,
-          spaceBetween: 20,
-          slidesPerView: 1.1,
-          width: 350,
-          pagination: {
-            el: '.our_story_pagination',
-            clickable: true,
-            renderBullet: (index, className) => {
-              return '<span class="ourStory_pagination ' + className + '"></span>';
-            }
-          },
-          on: {
-            init() {
-              var storys = Array.from(document.querySelectorAll('.storys'));
-              storys.forEach((s, i) => {
-                s.onclick = () => {
-                  wi_swiper.slideTo(i)
-                }
-              })
-            }
           }
-        })
-      },
-      slideChangeTransitionStart: function () {
-        swiperAnimate(this);
-      }
+        }
+      })
+    },
+    slideChangeTransitionStart: function () {
+      swiperAnimate(this);
     }
-  })
-  
+  }
+})
 
+
+
+var phones = Array.from(document.querySelectorAll('.phone'))
+phones.forEach((p, i) => {
+  p.onclick = () => {
+    var number = p.getAttribute('title')
+    window.open(number);
+  }
+})
+window.onload = () => {
   
-  var phones = Array.from(document.querySelectorAll('.phone'))
-  phones.forEach((p, i) => {
-    p.onclick = () => {
-      var number = p.getAttribute('title')
-      window.open(number);
-    }
-  })
 
 }
